84bf6bc46af80070f5fde089d79e4b8b59f8b799
Matthew Winder <mwinder@users.noreply.github.com>
2018-03-12 21:09:27 +0000

    Inital setup

---
+++ CodeFirstSample/BloggingContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+
+namespace CodeFirstSample
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
